287015880c9d928ff8cd333d3aec1e4a26c35b58
CaioJoseGP <163907108+CaioJoseGP@users.noreply.github.com>
2026-03-10 21:38:55 -0300

    update main

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-    logger1 = Logger.getInstance();
-    logger1.log("testes sla aslalsmdadmsaldsa");
-    
-    Logger logger2 = Logger.getInstance();
-    logger2.log("esse eh o 2 vamo ve se trava hehehehe");
-    System.out.println("São iguais? " + (logger1 == logger2));
+        Logger logger1 = Logger.getInstance();
+        logger1.log("testes sla aslalsmdadmsaldsa");
+        
+        Logger logger2 = Logger.getInstance();
+        logger2.log("esse eh o 2 vamo ve se trava hehehehe");
+        
+        System.out.println("São iguais? " + (logger1 == logger2));
     }
 }
